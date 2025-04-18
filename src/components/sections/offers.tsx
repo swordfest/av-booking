ee45779e569bd7f398f5521413aeb3ec78d8878b
swordfest <alexanderhavarro@gmail.com>
2025-04-18 15:01:12 -0400

    add card component to offers section and improve layout

--- src/components/sections/offers.tsx
+++ src/components/sections/offers.tsx
@@ -1,7 +1,8 @@
-import { Box, Stack, Typography } from "@mui/material";
+import { Box, Card, Stack, Typography } from "@mui/material";
 import CardOffer from "@/components/card/card-offer";
 import { cardOne, cardTwo } from "@/_mock/_offers";
- const Offers = () => {
+import { orange } from "@mui/material/colors";
+const Offers = () => {
   return (
     <Box
       title="Offers"
@@ -11,13 +12,62 @@ import { cardOne, cardTwo } from "@/_mock/_offers";
       gap={2}
     >
       <Stack direction={"column"}>
-        <Typography variant="h4" fontWeight={"700"}>Ofertas</Typography>
+        <Typography variant="h4" fontWeight={"700"}>
+          Ofertas
+        </Typography>
         <Typography variant="body1">Promociones y ofertas para ti</Typography>
       </Stack>
       <Stack maxWidth={"lg"} direction={{ md: "row", lg: "row" }} gap={2}>
         <CardOffer {...cardOne} />
         <CardOffer {...cardTwo} />
       </Stack>
+      <Card
+        sx={{
+          width: "20%",
+          backgroundColor: "orange",
+          cursor: "pointer",
+        }}
+      >
+        <Box>
+          <Box
+            sx={{
+              display: "flex",
+              alignItems: "start",
+              justifyContent: "space-between",
+              py: 2,
+              px: 2.5,
+              height: "auto",
+            }}
+          >
+            <Typography
+              gutterBottom
+              variant="h5"
+              fontWeight={600}
+              fontSize={"18px"}
+              margin={0}
+              sx={{
+                color: "white",
+                lineHeight: "90%",
+              }}
+            >
+              Nave 1
+            </Typography>
+            <Typography
+              gutterBottom
+              variant="h5"
+              fontWeight={500}
+              fontSize={"48px"}
+              margin={0}
+              sx={{
+                color: "white",
+                lineHeight: "75%",
+              }}
+            >
+              78%
+            </Typography>
+          </Box>
+        </Box>
+      </Card>
     </Box>
   );
 };
